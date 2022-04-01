99aa2692bbc219fbf4e0c6d58206bd4c283dba73
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-03-31 21:14:43 -0600

    add arduino sketch to map joystick

--- sketch_mar30a/sketch_mar30a.ino
+++ sketch_mar30a/sketch_mar30a.ino
@@ -1,9 +1,27 @@
+//maping out input from joysticks
+
+const int readJoyStickOneX = 0;
+const int readJoyStickOneY = 1;
+const int readJoyStickOneButton = 2;
+
+int joyStickOneXValue = 0;
+int joyStickOneYValue = 0;
+int joyStickOneButtonValue = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  joyStickOneXValue =analogRead(readJoyStickOneX);
+  joyStickOneYValue =analogRead(readJoyStickOneY);
+  joyStickOneButtonValue = digitalRead(readJoyStickOneButton);
 
+  Serial.print(joyStickOneYValue);
+  Serial.print("\t");
+  Serial.print(joyStickOneXValue);
+  Serial.print("\t");
+  Serial.println(joyStickOneButtonValue);
+  
 }
