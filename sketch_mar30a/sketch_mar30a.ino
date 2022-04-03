939076a16551f18bd1aa73ce1b8688fdfacfa5ec
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-04-03 17:17:10 -0600

    could not get button to work switched to building out and testing some servo functionality

--- sketch_mar30a/sketch_mar30a.ino
+++ sketch_mar30a/sketch_mar30a.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 //maping out input from joysticks
 
 const int readJoyStickOneX = 0;
@@ -6,22 +8,49 @@ const int readJoyStickOneButton = 2;
 
 int joyStickOneXValue = 0;
 int joyStickOneYValue = 0;
-int joyStickOneButtonValue = 0;
+int joyStickOneButtonState = HIGH;
 
-void setup() {
+//Robot posistion by axis
+//int axisOnePosistion = 0;
+//int axisTwoPosistion = 0;
+
+//Robot Servos and there assigned axis
+Servo axisOneServo;
+Servo axisTwoServo;
+
+void setup() { 
+  // initialize the pushbutton pin as input
+  pinMode(readJoyStickOneButton, INPUT);
+
+
+  //Assign Servo to pins 
+  axisOneServo.attach(3);
+  axisTwoServo.attach(5);
   Serial.begin(9600);
 
 }
 
 void loop() {
-  joyStickOneXValue =analogRead(readJoyStickOneX);
-  joyStickOneYValue =analogRead(readJoyStickOneY);
-  joyStickOneButtonValue = digitalRead(readJoyStickOneButton);
 
-  Serial.print(joyStickOneYValue);
-  Serial.print("\t");
+  
+
+  joyStickOneXValue = analogRead(readJoyStickOneX);
+  joyStickOneXValue = map(joyStickOneXValue, 0, 1023, 0, 180);
+  joyStickOneYValue = analogRead(readJoyStickOneY);
+  joyStickOneYValue = map(joyStickOneYValue, 0, 1023, 0, 180);
+  
+  joyStickOneButtonState = digitalRead(readJoyStickOneButton);
+
+  
+  axisOneServo.write(joyStickOneXValue);
+  delay(15);
+  axisTwoServo.write(joyStickOneYValue);
+  delay(15);
+
   Serial.print(joyStickOneXValue);
   Serial.print("\t");
-  Serial.println(joyStickOneButtonValue);
+  Serial.print(joyStickOneYValue);
+  Serial.print("\t");
+  Serial.println(joyStickOneButtonState);
   
 }
